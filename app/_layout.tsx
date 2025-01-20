6fce7724a2d95ff396d2f7571094c840d8ac3f05
HoangVietPW26 <nhviet26@gmail.com>
2025-01-20 15:53:00 +0000

    Sign In UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { SplashScreen, Stack } from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+// Import your global CSS file
+import "./globals.css";
+import {useFonts} from "expo-font";
+import { useEffect } from "react";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [fontsLoaded] = useFonts({
+    "Rubic-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubic-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubic-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubic-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubic-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubic-Light": require("../assets/fonts/Rubik-Light.ttf"),
+  })
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
     }
-  }, [loaded]);
+  }, [fontsLoaded])
 
-  if (!loaded) {
-    return null;
+  if (!fontsLoaded) {
+    return null
   }
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
